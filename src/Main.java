bda42e30ec5957d39f078e3a7bb4788be8473731
WadeeSami <wadee.abuzant@live.com>
2017-05-10 19:27:28 +0200

    adding new animated files

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
@@ -9,15 +10,15 @@ import java.util.regex.Pattern;
 
 public class Main {
 
-	public static void main(String[] args) {
-
+	static void handleIssues(String fileName) {
 		// read the file
-		String fileName = "g9";
+		// String fileName = "g9";
 		String line = null;
 		String finalResult = "";
 		try {
+
 			// FileReader reads text files in the default encoding.
-			FileReader fileReader = new FileReader(fileName+ ".txt");
+			FileReader fileReader = new FileReader(fileName);
 
 			// Always wrap FileReader in BufferedReader.
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
@@ -32,7 +33,8 @@ public class Main {
 			int result[] = new int[4];
 
 			for (int i = 0; i < splittedItems.length; i++) {
-				Matcher matcher = Pattern.compile("[0-9]+").matcher(splittedItems[i]);
+				Matcher matcher = Pattern.compile("[0-9]+").matcher(
+						splittedItems[i]);
 				int j = 0;
 				while (matcher.find()) {
 					// Do something with the matched text
@@ -44,20 +46,24 @@ public class Main {
 				int r = result[1] / 16;
 				int g = result[2] / 16;
 				int b = result[3] / 16;
-				finalResult += "LED( " + rowIndex + ", " + colIndex + ", " + r + ", " + g + ", " + b + ");\n";
+				finalResult += "LED( " + rowIndex + ", " + colIndex + ", " + r
+						+ ", " + g + ", " + b + ");\n";
 			}
 
 			System.out.println(finalResult);
 
 			try {
-	            BufferedWriter out = new BufferedWriter(new FileWriter(fileName + "-res.txt"));
-	            out.write(finalResult);
-	            out.close();
-	        }
-	        catch (IOException e)
-	        {
-	            System.out.println("Exception ");       
-	        }
+				String newFileName = "";
+				if (fileName.indexOf(".") > 0)
+					newFileName = fileData.substring(0,
+							fileName.lastIndexOf("."));
+				BufferedWriter out = new BufferedWriter(new FileWriter(fileName
+						+ "-res.txt"));
+				out.write(finalResult);
+				out.close();
+			} catch (IOException e) {
+				System.out.println("Exception ");
+			}
 
 			// Always close files.
 			bufferedReader.close();
@@ -68,5 +74,25 @@ public class Main {
 			// Or we could just do this:
 			// ex.printStackTrace();
 		}
+	}
+
+	public static void main(String[] args) {
+
+		File folder = new File("./");
+		File[] listOfFiles = folder.listFiles();
+
+		for (int i = 0; i < listOfFiles.length; i++) {
+			if (listOfFiles[i].isFile()) {
+				if(listOfFiles[i].getName().endsWith(".txt")){
+					System.out.println(listOfFiles[i].getName());
+
+				handleIssues(listOfFiles[i].getName());
+				}
+			} else if (listOfFiles[i].isDirectory()) {
+//				System.out.println("Directory " + listOfFiles[i].getName());
+			}
+		}
+
+		
 	}
 }
