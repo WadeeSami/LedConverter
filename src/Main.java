e691c5c6d8fc51120f0554092bc07d2fe7e4cc98
WadeeSami <wadee.abuzant@live.com>
2017-05-10 10:34:12 +0200

    adding grid files

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ public class Main {
 	public static void main(String[] args) {
 
 		// read the file
-		String fileName = "1";
+		String fileName = "g9";
 		String line = null;
 		String finalResult = "";
 		try {
